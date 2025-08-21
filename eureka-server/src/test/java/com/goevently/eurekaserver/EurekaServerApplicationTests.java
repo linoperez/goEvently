6e8e915306f1fc3dda36d2d2028e984ed8269f60
lino <sks.samal.com@gmail.com>
2025-08-21 20:10:32 +0530

    implemented eureka server

---
+++ eureka-server/src/test/java/com/goevently/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goevently.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
